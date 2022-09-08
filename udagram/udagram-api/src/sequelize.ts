a579853efcfa0cb884f65a60b1c399c2b89950cd
hazemomran <66013681+hazemomran@users.noreply.github.com>
2022-09-08 09:03:54 +0200

    update

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
   host: config.host,
+  username: config.username,
+  password: 'thispass',
+  database: config.database,
 
   dialect: "postgres",
   storage: ":memory:",
--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://myproject2.us-east-1.elasticbeanstalk.co//api/v0",
 };
 
 /*
